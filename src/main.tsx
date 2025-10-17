0e5a5b1fbdd9ad3519f8272b11a814ea9facfbad
Carlos J. de la Torre <carlos.delatorremon@alum.uca.es>
2025-10-17 14:34:51 +0200

    GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -24,7 +24,7 @@ function Root() {
   return (
     <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
       <CssBaseline />
-      <BrowserRouter>
+      <BrowserRouter basename="/portfolio">
         <App darkMode={darkMode} setDarkMode={setDarkMode} />
       </BrowserRouter>
     </ThemeProvider>
